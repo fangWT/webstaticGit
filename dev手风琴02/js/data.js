1dc6c1c3da32c46f01e50363ae26b046904cb8e4
fangWT <lingchen991@163.com>
2019-08-01 14:17:06 +0800

    webstaticgit-zero

---
+++ dev手风琴02/js/data.js
@@ -0,0 +1,104 @@
+var horiObj = {},varObj={};
+horiObj.imageObj = [
+          {
+          "src":"image/1.jpg",
+          "url":"https://www.baidu.com",
+          "title":"Mercedes-Benz",
+          "icon":"fa-plane",
+          "content":"The German car brand Mercedes Benz, a hundred years of civilization, automobile pioneer and leader, is considered one of the most successful luxury car brand, its exquisite technology and excellent reach the peak of perfection of the quality standards, the new innovation ability, as well as a series of classic coupe style admirable."
+          },
+          {
+          "src":"image/2.jpg",
+          "url":"",
+          "title":"Real estate",
+          "icon":"fa-dashboard",
+          "content":"Real estate is called real estate in economics because of its own characteristics, that is, the immobility and immobility of position. There are three forms of existence: land, buildings and premises. In the real estate auction, the auction subject can also have three forms, namely land (or land use rights), the material entity and the rights and interests of the building and the premises in one state. With the development of personal property ownership, real estate has become a major component of commercial transactions."
+          },
+          {
+          "src":"image/3.jpg",
+          "url":"https://www.baidu.com",
+          "title":"Tourism",
+          "icon":"fa-globe",
+          "content":'"Trip" travel, go out, in order to achieve a goal in space from a to B in the process; "you" is traveling, sightseeing and entertainment, is to make these to travel. The combination of the two is tourism. Therefore, travel is more important than traveling. Tourism has not only "travel", but also "sightseeing" and entertainment.'
+          },
+          {
+          "src":"image/4.jpg",
+          "url":"",
+          "title":"Basketball",
+          "icon":"fa-tasks",
+          "content":"NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league. It was founded in New York in June 6, 1946. The men's Professional Basketball League composed of thirty teams in North America, one of the four major sports leagues in the United States, brings together the top players in the world."
+          },
+          {
+          "src":"image/5.jpg",
+          "url":"",
+          "title":"description",
+          "icon":"fa-rss-square",
+          "content":"NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league. It was founded in New York in June 6, 1946. The men's Professional Basketball League composed of thirty teams in North America, one of the four major sports leagues in the United States, brings together the top players in the world."
+          },
+          {
+          "src":"image/6.jpg",
+          "url":"",
+          "title":"Real estate",
+          "icon":"fa-th-large",
+          "content":"Real estate is called real estate in economics because of its own characteristics, that is, the immobility and immobility of position. There are three forms of existence: land, buildings and premises. In the real estate auction, the auction subject can also have three forms, namely land (or land use rights), the material entity and the rights and interests of the building and the premises in one state. With the development of personal property ownership, real estate has become a major component of commercial transactions."
+          },
+          {
+          "src":"image/7.jpg",
+          "url":"https://www.baidu.com",
+          "title":"Mercedes-Benz",
+          "icon":"fa-windows",
+          "content":"NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league. It was founded in New York in June 6, 1946. The men's Professional Basketball League composed of thirty teams in North America, one of the four major sports leagues in the United States, brings together the top players in the world."
+          },
+          {
+          "src":"image/8.jpg",
+          "url":"",
+          "title":"Real estate",
+          "icon":"fa-apple",
+          "content":"Real estate is called real estate in economics because of its own characteristics, that is, the immobility and immobility of position. There are three forms of existence: land, buildings and premises. In the real estate auction, the auction subject can also have three forms, namely land (or land use rights), the material entity and the rights and interests of the building and the premises in one state. With the development of personal property ownership, real estate has become a major component of commercial transactions."
+          }
+        ];
+
+
+varObj.imageObj = [
+          {
+          "src":"image/ver-1.jpg",
+          "url":"https://www.baidu.com",
+          "title":"Mercedes-Benz",
+          "icon":"fa-plane",
+          "content":"The German car brand Mercedes Benz, a hundred years of civilization, automobile pioneer and leader"
+          },
+          {
+          "src":"image/ver-2.jpg",
+          "url":"",
+          "title":"Real estate",
+          "icon":"fa-dashboard",
+          "content":"Real estate is called real estate in economics because of its own characteristics"
+          },
+          {
+          "src":"image/ver-3.jpg",
+          "url":"https://www.baidu.com",
+          "title":"Tourism",
+          "icon":"fa-globe",
+          "content":'"Trip" travel, go out, in order to achieve a goal in space from a to B in the process;'
+          },
+          {
+          "src":"image/ver-4.jpg",
+          "url":"",
+          "title":"Basketball",
+          "icon":"fa-tasks",
+          "content":"NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league."
+          },
+          {
+          "src":"image/ver-5.jpg",
+          "url":"",
+          "title":"description",
+          "icon":"fa-rss-square",
+          "content":"NBA（National Basketball Association）is the abbreviation of the American men's professional basketball league."
+          },
+          {
+          "src":"image/ver-6.jpg",
+          "url":"",
+          "title":"Real estate",
+          "icon":"fa-th-large",
+          "content":"Real estate is called real estate in economics because of its own characteristics, that is, the immobility and immobility of position. There are three forms of existence: land, buildings and premises. real estate has become a major component of commercial transactions."
+          }];
